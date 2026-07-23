8ce4282751be67da1abe3d2632d8b6c1f264680c
Jacob Titong <titongjacob@gmail.com>
2026-07-23 20:58:20 +0800

    Initialize constructor for Book class

--- Book.java
+++ Book.java
@@ -3,5 +3,9 @@ public class Book {
     public String author;
     public int year;
 
-    
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
 }
