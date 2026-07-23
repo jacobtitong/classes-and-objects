c9eb19ed084f16994a0fb729a6e926010f74a003
Jacob Titong <titongjacob@gmail.com>
2026-07-23 20:55:08 +0800

    Add starter variables for Book class

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
 public class Main {
+    static Book[] library = new Book[1];
+    static int numberOfBooks = library.length - 1;
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         boolean endProgram = false;
 
         while (!endProgram) {
