b60638179e0a59ba4757bd2641b644437d0f632c
Jacob Titong <titongjacob@gmail.com>
2026-07-23 20:46:07 +0800

    Initialize java starter code for library system

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean endProgram = false;
+
+        while (!endProgram) {
+            switch (option) {
+                case 1:
+                    break;
+                case 4:
+                    endProgram = true;
+                    break;
+            }
+        }
+    }
+}
