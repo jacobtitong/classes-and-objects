f8e85544b62892092e742e14f9d62d5641e9cc0c
Jacob Titong <titongjacob@gmail.com>
2026-07-24 10:17:36 +0800

    Create ID generator feature in Book class

--- Book.java
+++ Book.java
@@ -1,11 +1,18 @@
 public class Book {
+    public static int idCounter = 0;
+    public int id;
     public String title;
     public String author;
     public int year;
 
     public Book(String title, String author, int year) {
+        this.id = generateID();
         this.title = title;
         this.author = author;
         this.year = year;
+    }
+
+    public int generateID() {
+        return idCounter++;
     }
 }
